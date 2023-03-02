97eb9bacdfa5d701695050eb06270ec2d095f543
Prabin <prabinsharma120@gmail.com>
2023-03-02 23:23:04 +0530

    Debug to false  and allow host to *

--- codeEditor/settings.py
+++ codeEditor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u^2%kn9m(5eeznu!tle$a2fl$k1yy^(x$4i3jc*)3(wv1i96t5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
