1eea8c0fe40a3fe6d394cb055055db28feac8969
Prabin120 <87239578+Prabin120@users.noreply.github.com>
2023-03-06 13:10:16 +0530

    Update settings.py

--- codeEditor/settings.py
+++ codeEditor/settings.py
@@ -124,8 +124,9 @@ EMAIL_HOST_PASSWORD = 'fqbfgaysgxbjwoaa'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
